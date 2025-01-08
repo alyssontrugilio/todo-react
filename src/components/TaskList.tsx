a30f4eddbbd25015d572ca89a7de6fdfae109d93
AlyssonTrugilio <alysson.trugilio.dev@gmail.com>
2025-01-08 15:13:11 -0300

    feat: editando e deletando tarefas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -6,20 +6,38 @@ import styles from "./TaskList.module.css";
 
 interface Props {
   taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdit(task: ITask): void;
 }
 
-const TaskList = ({ taskList }: Props) => {
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
   return (
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            {" "}
-            <p>{task.title}</p>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i
+                className="bi bi-pencil"
+                onClick={() => {
+                  handleEdit(task);
+                }}
+              ></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
+            </div>
           </div>
         ))
       ) : (
-        <p> Não tem </p>
+        <p> Não há tarefas cadastradas! </p>
       )}
     </>
   );
